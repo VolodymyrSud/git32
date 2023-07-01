21c823d5350c1edce8b34170b972d7101132e1fd
Volodymyr Sudomliak <volodymyrsudomliak@gmail.com>
2023-07-02 00:49:17 +0300

    add classes MoveOn3PointsKey MoveOn4PointsKey MoveOn5PointsKey

---
+++ src/Keys/MoveOn3PointsKey.java
@@ -0,0 +1,11 @@
+package Keys;
+
+import java.io.File;
+
+public class MoveOn3PointsKey extends Key {
+
+    @Override
+    public String encode(File file) {
+        return super.encode(file);
+    }
+}
---
+++ src/Keys/MoveOn5PointsKey.java
@@ -0,0 +1,11 @@
+package Keys;
+
+import java.io.File;
+
+public class MoveOn5PointsKey extends Key {
+
+    @Override
+    public String encode(File file) {
+        return super.encode(file);
+    }
+}
